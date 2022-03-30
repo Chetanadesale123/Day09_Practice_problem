63b92e43422d08ab69de931a584bda44f8036f1c
59114_Chetana <deslechetna1999@gmail.com>
2022-03-30 18:40:37 +0530

    emp attendance and calculate emp daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,8 +6,10 @@ WAGE_PER_HOUR=20;
 ispresent=$((RANDOM%2));
 if [ $ispresent -eq $PRESENT ]
 then
+	echo "is present";
 	workingHour=8;
 else
+	echo "is absent";
 	workingHour=0;
 fi
 wage=$((WAGE_PER_HOUR*workingHour));
