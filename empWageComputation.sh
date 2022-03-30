f40922a0d914204cafd597d43c6d163ea4a9080f
59114_Chetana <deslechetna1999@gmail.com>
2022-03-30 19:20:32 +0530

    Solving using switch case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,21 +7,26 @@ WAGE_PER_HOUR=20;
 
 isPresent=$((RANDOM%3));
 
-echo $isPresent;
+case $isPresent in
 
-if [ $isPresent -eq $PRESENT ]
-then
-	echo "Employee is present";
-	workingHour=8;
-elif [ $isPresent -eq $PART_TIME ]
-then
-	echo "Employee is working part time";
-	workingHour=4;
-else
-	echo "Employee is absent";
-	workingHour=0;
-fi
+	$PRESENT)
+		echo "Employee is present";
+		workingHour=8;
+	;;
+
+	$PART_TIME)
+		echo "Employee is working part time";
+		workingHour=4;
+	;;
+
+	*)
+		echo "Employee is absent";
+		workingHour=0;
+	;;
+
+esac;
 
 dailyWage=$((workingHour * WAGE_PER_HOUR));
 
 echo "Employee daily wage : $"$dailyWage "USD";
+
