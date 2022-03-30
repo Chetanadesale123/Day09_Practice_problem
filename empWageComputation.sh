1124b08eff8698a92d9c575485f7d626ceedd77c
59114_Chetana <deslechetna1999@gmail.com>
2022-03-30 19:38:21 +0530

    calculate wages for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,29 +4,34 @@ echo "welcome";
 PRESENT=1;
 PART_TIME=2;
 WAGE_PER_HOUR=20;
+MaX_Working_Day=20;
+totalSalary=0;
+for ((day=0;day<$MaX_Working_Day;day++))
+do
+	isPresent=$((RANDOM%3));
 
-isPresent=$((RANDOM%3));
+	case $isPresent in
 
-case $isPresent in
-
-	$PRESENT)
+		$PRESENT)
 		echo "Employee is present";
 		workingHour=8;
 	;;
 
-	$PART_TIME)
+		$PART_TIME)
 		echo "Employee is working part time";
 		workingHour=4;
 	;;
 
-	*)
+		*)
 		echo "Employee is absent";
 		workingHour=0;
 	;;
 
 esac;
 
-dailyWage=$((workingHour * WAGE_PER_HOUR));
-
+	dailyWage=$((workingHour * WAGE_PER_HOUR));
+	totalsalary=$((totalsalary+dailyWage));
+done
 echo "Employee daily wage : $"$dailyWage "USD";
+echo "Employee monthly wage : $"$totalsalary"USD";
 
