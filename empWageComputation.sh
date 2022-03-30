b584ad51fc0aa6f3a812aec5b672d4bd26855205
59114_Chetana <deslechetna1999@gmail.com>
2022-03-31 01:05:26 +0530

    write a function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,53 +1,49 @@
-#!bin/bash/
+#!/bin/bash
 
-echo "welcome";
 PRESENT=1;
 PART_TIME=2;
 WAGE_PER_HOUR=20;
 MAX_WORKING_DAY=20;
 MAX_WORKING_HOUR=40;
-totalWorkingHour=0;
-day=0;
 
-while [[ $day -lt $MAX_WORKING_DAY && $totalWorkingHour -lt $MAX_WORKING_HOUR ]]
-
-do
-	if [ $((totalWorkingHour + 4)) -ge $MAX_WORKING_HOUR ]
-then 
-	isprsent=$PART_TIME;
-else	
-	isPresent=$((RANDOM%3));
-fi
-	case $isPresent in
+function getWorkingHour() {
+	case $1 in
 
 		$PRESENT)
-		echo "Employee is present";
-		workingHour=8;
-	;;
+			wHour=8;
+		;;
 
 		$PART_TIME)
-		echo "Employee is working part time";
-		workingHour=4;
-	;;
+			wHour=4;
+		;;
 
 		*)
-		echo "Employee is absent";
-		workingHour=0;
-	;;
+			wHour=0;
+		;;
+	esac;
 
-esac;
-	dailywage=$((workingHour * WAGE_PER_HOUR))
+	echo $wHour;
+}
+
+totalWorkingHour=0;
+
+day=0;
+while [[ $day -lt $MAX_WORKING_DAY && $totalWorkingHour -lt $MAX_WORKING_HOUR ]]
+do
+	if [ $((totalWorkingHour + 4)) -ge $MAX_WORKING_HOUR ]
+	then
+		isPresent=$PART_TIME;
+	else
+		isPresent=$((RANDOM%3));
+	fi
+
+	workingHour=$(getWorkingHour $isPresent);
 	totalWorkingHour=$((totalWorkingHour+workingHour));
-	totalSalary=$((totalsalary+dailywage));
-
 	((day++));
 done
-		
 
-	echo "Employee monthly wage :  $totalsalary USD";
-  echo "Employee daily wage :  $dailywage USD";
-	totalSalary=$((totalWorkingHour*WAGE_PER_HOUR));
+totalSalary=$((totalWorkingHour*WAGE_PER_HOUR));
 
-
+echo "Employee monthly wage : $"$totalSalary "USD";
 echo "Total working hour : $totalWorkingHour Hr";
 echo "Total working day : $day";
